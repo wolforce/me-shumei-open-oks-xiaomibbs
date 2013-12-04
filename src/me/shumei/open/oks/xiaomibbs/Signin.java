ed0e3c2a736b0f9b37dbb55f306ee57a987c5466
shumei <wolforced@gmail.com>
2013-12-05 01:15:36 +0800

    Change program to fit the signin page's revision

--- src/me/shumei/open/oks/xiaomibbs/Signin.java
+++ src/me/shumei/open/oks/xiaomibbs/Signin.java
@@ -3,9 +3,11 @@ package me.shumei.open.oks.xiaomibbs;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.json.JSONObject;
 import org.jsoup.Connection.Method;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import android.content.Context;
@@ -46,9 +48,11 @@ public class Signin extends CommonData {
             //登录BBS的URL
             String loginBBSUrl = "http://bbs.xiaomi.cn/member.php?mod=logging&action=login&mobile=yes";
             //签到页面的URL
-            String signPageUrl = "http://bbs.xiaomi.cn/plugin.php?id=mi_sign:sign";
+            String signPageUrl = "http://bbs.xiaomi.cn/qiandao/";
+            //获取签到口号的URL
+            String logoUrl = "http://oss.xiaomi.com/cmsApi/logo/GetLogo";
             //提交签到信息的URL
-            String signSubmitUrl = "http://bbs.xiaomi.cn/plugin.php?id=mi_sign:sign&operation=qiandao&infloat=0&inajax=0";
+            String signSubmitUrl = "http://bbs.xiaomi.cn/qiandao/index/share";
             
             //登录小米通行证账号
             res = Jsoup.connect(loginPassportUrl).data("user", user).data("pwd", pwd).data("sid", "passport").userAgent(UA_ANDROID).timeout(TIME_OUT).ignoreContentType(true).method(Method.POST).execute();
@@ -62,57 +66,67 @@ public class Signin extends CommonData {
                 return new String[]{this.resultFlag,this.resultStr};
             }
             
-            
             //带着通行证的Cookies访问论坛地址，实现登录论坛
             res = Jsoup.connect(loginBBSUrl).cookies(cookies).userAgent(UA_ANDROID).timeout(TIME_OUT).ignoreContentType(true).method(Method.GET).execute();
             cookies.putAll(res.cookies());
-            
-            //要发的表情，共9个
-            //开心，难过，郁闷，无聊，怒，擦汗，奋斗，慵懒，衰
-            String[] qdxqArr = {"kx","ng","ym","wl","nu","ch","fd","yl","shuai"};
-            
-            //要说的话，共7个
-            //{"我爱小米社区，社区是我家！","因为米粉，所以小米！","米粉给力！小米加油！","米粉们，我来啦！","好好签到，天天向上！","哎...今天够累的，签到来啦！","好好学习，天天向上！","我是小米发烧友，我爱小米！"};
-            String[] fastreplyArr = {"1","2","3","4","5","6","7","8"};
-            
-            int randNum;
-            randNum = (int)(Math.random() * 8);
-            String qdxq = qdxqArr[randNum];
-            randNum = (int)(Math.random() * 7);
-            String fastreply = fastreplyArr[randNum];
             
             //访问电脑版签到页面
             res = Jsoup.connect(signPageUrl).cookies(cookies).userAgent(UA_CHROME).timeout(TIME_OUT).ignoreContentType(true).method(Method.GET).execute();
             cookies.putAll(res.cookies());
             
             //分析签到页面的信息，判断今天是否已经签到
-            if(res.parse().toString().contains("今天已经签到过了或者签到时间还未开始"))
+            if(res.parse().toString().contains("您今天还没有签到"))
             {
-                resultFlag = "true";
-                //捕捉一下异常，防止获取数据时发生错误
-                try {
-                    Elements paragraphs = res.parse().select("#ct .mn > p");
-                    resultStr = "今天已签过到\n";
-                    resultStr += paragraphs.eq(0).text() + "\n";
-                    resultStr += paragraphs.eq(1).text() + "\n";
-                    resultStr += paragraphs.eq(2).text() + "\n";
-                    resultStr += paragraphs.eq(3).text() + "\n";
-                    resultStr += paragraphs.eq(4).text() + "\n";
-                } catch (Exception e) {
-                    resultStr = "今天已签到";
+                //获取签到口号
+                //var logo_url = "";var logo_text = "投资未来的人是忠于现实的人。";var logo_text_count=33124;
+                res = Jsoup.connect(logoUrl).cookies(cookies).userAgent(UA_CHROME).timeout(TIME_OUT).ignoreContentType(true).method(Method.GET).execute();
+                cookies.putAll(res.cookies());
+                String logoStr = res.body().replaceAll(".+var logo_text.*=.*\"(.+)\".*", "$1");
+                
+                //向电脑版网页提交签到信息
+                res = Jsoup.connect(signSubmitUrl).data("text", logoStr).cookies(cookies).userAgent(UA_CHROME).timeout(TIME_OUT).ignoreContentType(true).method(Method.POST).execute();
+                JSONObject jsonObj = new JSONObject(res.body());
+                String code = jsonObj.getString("code");
+                if (code.equals("200")) {
+                    JSONObject userObj = jsonObj.getJSONObject("user");
+                    this.resultFlag = "true";
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(userObj.getString("name") + ",签到成功\n");
+                    sb.append("连续签到：" + userObj.getString("days") + "\n");
+                    sb.append("签到等级：" + userObj.getString("level") + "\n");
+                    sb.append("获得奖励：" + userObj.getString("lastreward") + "\n");
+                    sb.append("积分奖励：" + userObj.getString("reward") + "\n");
+                    sb.append("签到排名：" + userObj.getString("sort") + "\n");
+                    sb.append("总天数：" + userObj.getString("tdays") + "\n");
+                    
+                    //中奖
+                    if (jsonObj.getString("data").length() > 0) {
+                        sb.append("签到中奖！中奖信息：\n" );
+                        sb.append(jsonObj.getString("data"));
+                    }
+                    
+                    this.resultStr = sb.toString();
+                } else {
+                    this.resultFlag = "false";
+                    this.resultStr = "登录成功，在提交签到信息时发生未知错误";
                 }
             }
             else
             {
-                //向电脑版网页提交签到信息
-                String formhash = res.parse().getElementsByAttributeValue("name", "formhash").first().val();
-                res = Jsoup.connect(signSubmitUrl).data("qdmode","2").data("formhash",formhash).data("qdxq",qdxq).data("fastreply",fastreply).data("todaysay","").cookies(cookies).userAgent(UA_CHROME).timeout(TIME_OUT).ignoreContentType(true).method(Method.POST).execute();
-                
-                this.resultFlag = "true";
+                resultFlag = "true";
+                //捕捉一下异常，防止获取数据时发生错误
                 try {
-                    this.resultStr = res.parse().select("#ct .c").text().replace("[点此返回]", "");
+                    Document doc = res.parse();
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("今天已签过到\n");
+                    sb.append("连续签到：" + doc.getElementById("lxdays").val() + "\n");
+                    sb.append("签到等级：" + doc.getElementById("lxlevel").val() + "\n");
+                    sb.append("积分奖励：" + doc.getElementById("lxreward").val() + "\n");
+                    sb.append("签到排名：" + doc.getElementById("qiandaobtnnum").val() + "\n");
+                    sb.append("总天数：" + doc.getElementById("lxtdays").val() + "\n");
+                    resultStr = sb.toString();
                 } catch (Exception e) {
-                    this.resultStr = "签到成功";
+                    resultStr = "今天已签到";
                 }
             }
             
